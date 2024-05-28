583bc51c5a340df634a8642f5a67068ba2ad8e6d
Сафронов Евгений <emsafronov2004@gmail.com>
2024-05-29 01:49:32 +0300

    style

--- app/lib/features/joke_screen/widgets/annotation_push.dart
+++ app/lib/features/joke_screen/widgets/annotation_push.dart
@@ -43,88 +43,94 @@ class _AnnotationPushWidgetState extends State<AnnotationPushWidget> {
               const Flexible(flex: 1, child: SizedBox.shrink()),
               Flexible(
                 flex: 3,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        color: AppColors.color800,
-                        borderRadius: BorderRadius.circular(25),
-                        border: Border.all(
-                          color: AppColors.color900,
-                          width: 2,
-                          style: BorderStyle.solid,
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: AppColors.color800,
+                          borderRadius: BorderRadius.circular(25),
+                          border: Border.all(
+                            color: AppColors.color900,
+                            width: 2,
+                            style: BorderStyle.solid,
+                          ),
                         ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Center(
-                              child: Text(
-                                'Добавление аннотации',
-                                style: TextStyle(
-                                  color: AppColors.color200,
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.w600,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Center(
+                                child: Text(
+                                  'Аннотация',
+                                  style: TextStyle(
+                                    color: AppColors.color200,
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w600,
+                                  ),
                                 ),
                               ),
-                            ),
-                            Text(
-                              widget.textToAnnotate,
-                              style: TextStyles.defaultJokeStyle.copyWith(
-                                color: AppColors.color200,
+                              Text(
+                                widget.textToAnnotate,
+                                style: TextStyles.defaultJokeStyle.copyWith(
+                                  color: AppColors.color200,
+                                ),
                               ),
-                            ),
-                            const SizedBox(height: 16),
-                            Material(
-                              child: TextFormField(
-                                controller: _annotationController,
+                              const SizedBox(height: 16),
+                              Material(
+                                color: AppColors.color400,
+                                child: TextField(
+                                  style: TextStyles.defaultJokeStyle,
+                                  controller: _annotationController,
+                                  keyboardType: TextInputType.multiline,
+                                  maxLines: null,
+                                ),
                               ),
-                            ),
-                            const SizedBox(height: 16),
-                            Center(
-                              child: SizedBox(
-                                width: 160,
-                                height: 60,
-                                child: OutlinedButton(
-                                  style: ButtonStyle(
-                                    side: WidgetStateProperty.all(
-                                      const BorderSide(
-                                        color: AppColors.color400,
-                                        width: 2.5,
+                              const SizedBox(height: 16),
+                              Center(
+                                child: SizedBox(
+                                  width: 160,
+                                  height: 60,
+                                  child: OutlinedButton(
+                                    style: ButtonStyle(
+                                      side: WidgetStateProperty.all(
+                                        const BorderSide(
+                                          color: AppColors.color400,
+                                          width: 2.5,
+                                        ),
+                                      ),
+                                    ),
+                                    onPressed: () async {
+                                      await context.read<AnnotationPushCubit>().pushAnnotation(
+                                            jokeId: widget.jokeId,
+                                            textFrom: widget.textFrom,
+                                            textTo: widget.textTo,
+                                            annotation: _annotationController.text,
+                                          );
+                                      Navigator.of(context).pop(true);
+                                    },
+                                    child: const Text(
+                                      textAlign: TextAlign.center,
+                                      'Предложить\nаннотацию',
+                                      style: TextStyle(
+                                        color: AppColors.color200,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w600,
                                       ),
                                     ),
                                   ),
-                                  onPressed: () async {
-                                    await context.read<AnnotationPushCubit>().pushAnnotation(
-                                          jokeId: widget.jokeId,
-                                          textFrom: widget.textFrom,
-                                          textTo: widget.textTo,
-                                          annotation: _annotationController.text,
-                                        );
-                                    Navigator.of(context).pop(true);
-                                  },
-                                  child: const Text(
-                                    textAlign: TextAlign.center,
-                                    'Предложить\nаннотацию',
-                                    style: TextStyle(
-                                      color: AppColors.color200,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                  ),
                                 ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ],
